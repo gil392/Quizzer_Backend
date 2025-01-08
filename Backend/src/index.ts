5b6fa4e508b72d6e5f9fdf8766d96a728bdb3c5c
Tomer Shomron <tomercpc01@gmail.com>
2025-01-08 23:19:32 +0200

    starting system

---
+++ Backend/src/index.ts
@@ -0,0 +1,18 @@
+import { getProcessEnv } from './config';
+import { createSystemConfig } from './services/system/config';
+import { System } from './services/system/system';
+
+const handleError = (error: Error) => {
+    console.error(error);
+    process.exit(1);
+};
+
+try {
+    const processEnv = getProcessEnv();
+    const systemConfig = createSystemConfig(processEnv);
+    const system = new System(systemConfig);
+
+    system.start().catch(handleError);
+} catch (error) {
+    handleError(error as Error);
+}
