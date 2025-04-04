47b47eb73c0220988343a5f1b4b5cc2a7c48c56f
Itay <itay.finkelstein@gmail.com>
2025-04-04 19:13:44 +0300

    added video details mettadata and captions extract functions

---
+++ src/services/server/youtube/utils.ts
@@ -0,0 +1,5 @@
+export const extractVideoId = (url: string): string | null => {
+    const regex = /(?:v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+};
