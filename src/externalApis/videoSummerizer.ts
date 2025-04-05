591d022963f5ba1b000aa6e88788491dfba6208c
omer hasid <omerhasid11@gmail.com>
2025-04-05 23:23:08 +0300

    add title to create lesson

--- src/externalApis/videoSummerizer.ts
+++ src/externalApis/videoSummerizer.ts
@@ -1,7 +1,6 @@
 import { SummarizerConfig } from "./transcriptSummarizer/config";
 import { Summarizer } from "./transcriptSummarizer/transcriptSummarizer";
 import { fetchVideoTranscript } from "./youtube/getCaptions";
-import { extractVideoId } from "./youtube/utils";
 
 export class VideoSummeraizer {
     private summarizer: Summarizer; 
@@ -10,8 +9,7 @@ export class VideoSummeraizer {
         this.summarizer = new Summarizer(summarizerConfig) 
     }
 
-    summerizeVideo = async (videoUrl: string): Promise<string> => {
-       const videoId = extractVideoId(videoUrl);
+    summerizeVideo = async (videoId: string): Promise<string> => {
         const videoTranscript = await fetchVideoTranscript(videoId);
         return videoTranscript ? this.summarizer.summarizeTranscript(videoTranscript) : '';
     }
