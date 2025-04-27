b8034e4cd8c914ab8ab71de64246b42efc9691c5
Tomer Shomron <tomercpc01@gmail.com>
2025-04-27 22:03:57 +0300

    fix typo

--- src/user/consts.ts
+++ src/user/consts.ts
@@ -1,7 +1,7 @@
 import { ProjectionType } from 'mongoose';
 import { User } from './model';
 
-export const EXLUDE_USER_PRIVATE_PROPERTIES_PROJECTION: ProjectionType<User> = {
+export const EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION: ProjectionType<User> = {
     hashedPassword: 0,
     refreshToken: 0
 };
