81156941a3d78b3988a51f27dd71af0b6900bc13
Tomer Shomron <tomercpc01@gmail.com>
2025-05-12 20:51:12 +0300

    dal search users

--- src/user/consts.ts
+++ src/user/consts.ts
@@ -1,7 +1,10 @@
-import { ProjectionType } from 'mongoose';
-import { User } from './model';
+import { ProjectionType } from "mongoose";
+import { User } from "./model";
 
-export const EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION: ProjectionType<User> = {
+export const EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION: ProjectionType<User> =
+  {
     hashedPassword: 0,
-    refreshToken: 0
-};
+    refreshToken: 0,
+  };
+
+export const SEARCH_USER_SELECT = "username email picture -_id";
--- src/user/dal.ts
+++ src/user/dal.ts
@@ -1,10 +1,24 @@
-import { BasicDal } from '../services/database/base.dal';
-import { EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION } from './consts';
-import { User } from './model';
+import { BasicDal } from "../services/database/base.dal";
+import {
+  EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION,
+  SEARCH_USER_SELECT,
+} from "./consts";
+import { User } from "./model";
 
 export class UsersDal extends BasicDal<User> {
-    findByUsername = (username: string) => this.model.findOne({ username });
+  findByUsername = (username: string) => this.model.findOne({ username });
 
-    findPublicUserById = (id: string) =>
-        this.findById(id, EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION);
+  findPublicUserById = (id: string) =>
+    this.findById(id, EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION);
+
+  searchUsers = (searchTerm: string, limit: number = 10) => {
+    const regex = new RegExp(searchTerm, "i");
+
+    return this.model
+      .find({
+        $or: [{ username: { $regex: regex } }, { email: { $regex: regex } }],
+      })
+      .select(SEARCH_USER_SELECT)
+      .limit(limit);
+  };
 }
