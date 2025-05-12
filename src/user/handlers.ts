001528bb060f5ab00d94eb7946543c9ee83d5ba4
Tomer Shomron <tomercpc01@gmail.com>
2025-05-12 20:52:35 +0300

    search users handler

--- src/user/handlers.ts
+++ src/user/handlers.ts
@@ -1,13 +1,27 @@
-import { validateAuthenticatedRequest } from '../authentication/validators';
-import { NotFoundError } from '../services/server/exceptions';
-import { UsersDal } from './dal';
+import { StatusCodes } from "http-status-codes";
+import { validateAuthenticatedRequest } from "../authentication/validators";
+import { NotFoundError } from "../services/server/exceptions";
+import { UsersDal } from "./dal";
+import {
+  validateAnswerFriendRequestRequest,
+  validateCreateFriendRequestRequest,
+  validateSearchUsersRequest,
+} from "./validators";
 
 export const getLoggedUser = (usersDal: UsersDal) =>
-    validateAuthenticatedRequest(async (request, response) => {
-        const { id: userId } = request.user;
-        const user = await usersDal.findPublicUserById(userId).lean();
-        if (!user) {
-            throw new NotFoundError('user not found');
-        }
-        response.json(user);
-    });
+  validateAuthenticatedRequest(async (request, response) => {
+    const { id: userId } = request.user;
+    const user = await usersDal.findPublicUserById(userId).lean();
+    if (!user) {
+      throw new NotFoundError("user not found");
+    }
+    response.json(user);
+  });
+
+export const searchUsers = (usersDal: UsersDal) =>
+  validateSearchUsersRequest(async (request, response) => {
+    const { q: searchTerm } = request.query;
+    const users = await usersDal.searchUsers(searchTerm);
+
+    response.json(users);
+  });
