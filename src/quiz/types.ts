5c2056746f2cc3dbb839432ae6e39abb1d1b7bdb
Omer Hasid <omerhasid11@gmail.com>
2025-05-23 20:08:08 +0300

    change checkType to feedbackType

--- src/quiz/types.ts
+++ src/quiz/types.ts
@@ -1,36 +1,36 @@
-import { Types } from 'mongoose';
+import { Types } from "mongoose";
 
-export const quizCheckTypes = ['onSubmit', 'onSelectAnswer'] as const;
+export const quizFeedbacks = ["onSubmit", "onSelectAnswer"] as const;
 
 export interface Quiz {
-    title: string;
-    lessonId: string;
-    questions: Question[];
-    grade?: number;
-    settings: QuizSettings;
+  title: string;
+  lessonId: string;
+  questions: Question[];
+  grade?: number;
+  settings: QuizSettings;
 }
 
 export type QuizSettings = {
-    checkType: (typeof quizCheckTypes)[number];
-    isRandomOrder: boolean;
-    maxQuestionCount: number;
-    solvingTimeMs: number;
+  feedbackType: (typeof quizFeedbacks)[number];
+  isRandomOrder: boolean;
+  maxQuestionCount: number;
+  solvingTimeMs: number;
 };
 
 export type Answer = string;
 
 export type Question = {
-    _id?: Types.ObjectId;
-    question: string;
-    incorrectAnswers: Answer[];
-    correctAnswer: Answer;
+  _id?: Types.ObjectId;
+  question: string;
+  incorrectAnswers: Answer[];
+  correctAnswer: Answer;
 };
 
 export type QuizResponseQuestion = {
-    _id: string;
-    text: string;
-    answers: Answer[];
+  _id: string;
+  text: string;
+  answers: Answer[];
 };
-export type QuizResponse = Omit<Quiz, 'questions'> & {
-    questions: QuizResponseQuestion[];
+export type QuizResponse = Omit<Quiz, "questions"> & {
+  questions: QuizResponseQuestion[];
 };
