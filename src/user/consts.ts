ad3ee36b07bf3e04c76e746778d804dd7e7d4570
Tomer Shomron <tomercpc01@gmail.com>
2025-05-24 22:15:01 +0300

    build tests for users routes

--- src/user/consts.ts
+++ src/user/consts.ts
@@ -1,18 +1,22 @@
-import { ProjectionType } from "mongoose";
+import { Document, ProjectionType } from "mongoose";
 import { User } from "./model";
 
-export const EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION: ProjectionType<User> =
-  {
-    hashedPassword: 0,
-    refreshToken: 0,
-  };
+export const EXCLUDE_USER_PRIVATE_PROPERTIES_PROJECTION = {
+  hashedPassword: 0,
+  refreshToken: 0,
+} satisfies ProjectionType<User>;
 
-export const USER_FRIENDS_PROJECTION: ProjectionType<User> = {
+export const USER_FRIENDS_PROJECTION = {
   _id: 1,
   email: 1,
   username: 1,
   favoriteLessons: 1,
   streak: 1,
-};
+} satisfies ProjectionType<User>;
 
-export const SEARCH_USER_SELECT = "username email picture _id";
+export const SEARCH_USER_SELECT_KEYS = [
+  "username",
+  "email",
+  "_id",
+] satisfies (keyof (User & Document))[];
+export const SEARCH_USER_SELECT = SEARCH_USER_SELECT_KEYS.join(" ");
